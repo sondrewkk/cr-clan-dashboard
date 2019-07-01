a2e09526b42f665ef5c9b5a198d0295bb7b1c071
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2019-07-01 23:38:05 +0200

    Removed helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Homepage</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    components: {
-      HelloWorld
-    }
+
   }
 </script>
