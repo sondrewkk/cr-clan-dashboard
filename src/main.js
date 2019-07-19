f090272506f435b7f5c631d3e99b1b21b6a337f3
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2019-07-19 02:39:23 +0200

    Fixed store path

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import { store } from './store/store'
+import { store } from './store'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+const http = Axios.create({
+  baseURL: 'http://localhost:8081/api'
+})
+
+Vue.prototype.$http = http
 
 new Vue({
   router,
