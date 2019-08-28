d596143dec5528f9c804ca1941e205386c21d947
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2019-08-29 00:01:27 +0200

    Adjustments for 2.0.11

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
 <script>
 
   export default {
-    name: 'CRCLANDASHBOARD'
+    name: 'CRCLANDASHBOARD',
+    created () {
+      this.$vuetify.theme.dark = true
+    }
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,71 @@
 <template>
-  <v-app class="grey lighten-4">
-    <navbar />
-    <v-content class="mx-4 mb-4">
-      <router-view />
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer" 
+      app
+      clipped
+    >
+      <v-list 
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="(item, i) in menuItems"
+          :key="i"
+          router
+          :to="item.route"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon" />
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!-- <v-toolbar-title>Application</v-toolbar-title> -->
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-  import Navbar from '@/components/Navbar'
 
   export default {
-    name: 'CRCLANDASHBOARD',
-    components: {
-      Navbar
-    },
-    data () {
-      return {
-      //
+    data: () => ({
+      drawer: null,
+      menuItems: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'person', text: 'Me', route: '/user' },
+        { icon: 'group', text: 'Clan', route: '/clan' },
+        { icon: 'device_hub', text: 'Family', route: '/family' },
+        { icon: 'insert_chart_outlined', text: 'Reports', route: '/reports' },
+        { icon: 'settings', text: 'Settings', route: '/settings' }
+      ]
+    }),
+    methods: {
+      async logout () {
+        try {
+          await this.$store.dispatch('userAuthentication/logout')
+          this.$router.push('/login')
+        } catch (err) { console.error(err) }
       }
     }
   }
